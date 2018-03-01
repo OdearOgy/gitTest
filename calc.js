48a18e12fc5f8a18a250c52b6b94232076d5d8df
OdearOgy <hovhan198@gmail.com>
2018-03-01 21:12:17 +0400

    Update Calc :)

--- calc.js
+++ calc.js
@@ -1,19 +1,29 @@
+// const myBtn = document.getElementById("myButton");
+// myBtn.addEventListener('click', (event) => {
+//   }
+// )
+
+
+
+
+
+
 
 const sumInt = (a, b) => {
   return Number(a) + Number(b);
-}; console.log(sumInt(5, 5));
+}
 
 const substractionInt = (a,b) => {
   return Number(a) - Number(b);
-}; console.log(substractionInt(5, 6));
+}
 
 const divisionInt = (a,b) => {
   return a / b;
-}; console.log(divisionInt(10,2));
+}
 
 const multiplicationInt = (a,b) => {
   return a * b;
-}; console.log(multiplicationInt(5,10));
+}
 
 const power = (num, pow) => {
   let result = 1;
@@ -21,29 +31,28 @@ const power = (num, pow) => {
     result *= num;
   }
   return result
-}; console.log(power(5,0));
-
+}
 
 const root = (num, pow) => {
   return num ** (pow ** -1);
-}; console.log(root(36,2));
+}
 
 const absolute = (a) => {
  return a < 0 ? -a : a;
 
-}; console.log(absolute(2));
+}
 
 const mod = (a, b) => {
   return a % b;
-}; console.log(mod(15,7));
+}
 
 const flipSign = (a) => {
   return -a;
-}; console.log(flipSign(2));
+}
 
 const percent = (a,b) => {
   return (a * b) /100;
-}; console.log(percent(300,20));
+}
 
 
 const log = (base, a) => {
